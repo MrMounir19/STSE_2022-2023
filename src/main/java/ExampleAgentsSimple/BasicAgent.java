9b84d01ff269ababfe93cefdb8fc98312e764106
anfeny <peetersanthony208@gmail.com>
2022-10-26 16:46:19 +0200

    Create BasicAgent
    Basic dummy agent for testing.

---
+++ src/main/java/ExampleAgentsSimple/BasicAgent.java
@@ -0,0 +1,13 @@
+package ExampleAgentsSimple;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import lejos.utility.Delay;
+
+public class BasicAgent extends Agent{
+    @Override
+    public void setup() {
+        return;
+    }
+}
