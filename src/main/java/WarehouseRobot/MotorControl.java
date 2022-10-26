92388be2855de67cd6fce0f2bc5d2ad3ed91840e
anfeny <peetersanthony208@gmail.com>
2022-10-26 16:02:38 +0200

    Create WarehouseRobot framework

---
+++ src/main/java/WarehouseRobot/MotorControl.java
@@ -0,0 +1,9 @@
+package WarehouseRobot;
+
+public class MotorControl {
+    static int speed = 0;
+
+    public static void stopMotors() {
+        // Stop both motors.
+    }
+}
---
+++ src/main/java/WarehouseRobot/RobComponents.java
@@ -0,0 +1,23 @@
+package WarehouseRobot;
+
+import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
+import ev3dev.sensors.ev3.EV3ColorSensor;
+import ev3dev.sensors.ev3.EV3UltrasonicSensor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+
+
+public class RobComponents {
+    static EV3LargeRegulatedMotor motorL = null;
+    static EV3LargeRegulatedMotor motorR = null;
+    static EV3UltrasonicSensor ussF = null;
+    static EV3UltrasonicSensor ussL = null;
+
+    public static void init() {
+        motorL = new EV3LargeRegulatedMotor(MotorPort.B);
+        motorR = new EV3LargeRegulatedMotor(MotorPort.C);
+        ussF = new EV3UltrasonicSensor(SensorPort.S1);
+        ussL = new EV3UltrasonicSensor(SensorPort.S2);
+    }
+}
