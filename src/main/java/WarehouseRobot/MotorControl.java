79392e7cff895722eae290d14a9f31b132441699
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2022-10-28 18:29:51 +0200

    Expand MotorControl with movement methods & member variables
    Speed is saved in the MotorControl class, as well as a number of static speed variables that can be reused throughout the project as default speeds. The class also has a modifier for each wheel we can use in case the motors aren't properly calibrated / we run into issues due to different weight / grip distributions.

--- src/main/java/WarehouseRobot/MotorControl.java
+++ src/main/java/WarehouseRobot/MotorControl.java
@@ -1,9 +1,78 @@
 package WarehouseRobot;
 
-public class MotorControl {
-    static int speed = 0;
+import lejos.utility.Delay;
 
+public class MotorControl {
+    static int speed = 600;
+    static final public int slowSpeed = 100;
+    static final public int mediumSpeed = 600;
+    static final public int fastSpeed = 1500;
+    static int leftWheelModifier = 1;
+    static int rightWheelModifier = 1;
+    static int controlDelayMs = 1;
+    static int invertControls = 1; // 1 or -1
+
+    /**
+     * @param speed speed to apply to both motors.
+     */
+    private static void applyToBoth(int speed) {
+        RobComponents.motorL.setSpeed(invertControls * speed * leftWheelModifier);
+        RobComponents.motorR.setSpeed(invertControls * speed * rightWheelModifier);
+        Delay.msDelay(controlDelayMs);
+    }
+
+    /**
+     * Sets both motors to the same speed.
+     * @param speed speed to apply to both motors.
+     */
+    public static void setSpeed(int speed) {
+        MotorControl.speed = speed;
+    }
+
+    /**
+     * Turns left in place.
+     */
+    public static void turnLeftInPlace() {
+        applyToBoth(speed);
+        RobComponents.motorL.backward();
+        RobComponents.motorR.forward();
+        Delay.msDelay(controlDelayMs);
+    }
+
+    /**
+     * Turns right in place.
+     */
+    public static void turnRightInPlace() {
+        applyToBoth(speed);
+        RobComponents.motorL.forward();
+        RobComponents.motorR.backward();
+        Delay.msDelay(controlDelayMs);
+    }
+
+    /**
+     * Move forward.
+     */
+    public static void moveForward() {
+        applyToBoth(speed);
+        RobComponents.motorL.forward();
+        RobComponents.motorR.forward();
+        Delay.msDelay(controlDelayMs);
+    }
+
+    /**
+     * Move backward.
+     */
+    public static void moveBackward() {
+        applyToBoth(speed);
+        RobComponents.motorL.backward();
+        RobComponents.motorR.backward();
+        Delay.msDelay(controlDelayMs);
+    }
+
+    /**
+     * Sets both motors to 0 speed, stopping the robot.
+     */
     public static void stopMotors() {
-        // Stop both motors.
+        applyToBoth(0);
     }
 }
