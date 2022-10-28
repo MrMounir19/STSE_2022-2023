b5d80cddd6a758338b4186d8145b980083bf5901
MaximGonnissen <33728489+MaximGonnissen@users.noreply.github.com>
2022-10-28 18:31:34 +0200

    Add default behaviour to CollisionAvoidanceAgent
    The Agent will drive forward until it detects a collision, upon which it should activate its collision avoidance behaviour.
    
    ** Needs testing in the field!

--- src/main/java/Agents/CollisionAvoidanceAgent.java
+++ src/main/java/Agents/CollisionAvoidanceAgent.java
@@ -1,18 +1,22 @@
 package Agents;
 
+import WarehouseRobot.MotorControl;
 import jade.core.Agent;
 
 import Behaviours.CollisionAvoidanceBehaviour;
 import jade.core.behaviours.CyclicBehaviour;
 
 public class CollisionAvoidanceAgent extends Agent {
+    // TODO: Initialise RobComponents before the agent container starts. (In the container start file)
     @Override
     protected void setup() {
         addBehaviour(new CollisionAvoidanceBehaviour());
         addBehaviour(new CyclicBehaviour() {
             @Override
             public void action() {
-                // Move forward
+                MotorControl.setSpeed(MotorControl.mediumSpeed);
+                MotorControl.moveForward();
+                block(5000);
             }
         });
     }
