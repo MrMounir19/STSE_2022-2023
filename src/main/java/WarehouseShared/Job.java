2a9fa5e9c9031067afd044c62c97e96807863a6c
ThimotyS <thimoty.smet@gmail.com>
2022-12-10 23:26:23 +0100

    Redesign Job
    Add ID to uniquely identify jobs
    Rework path to use array of Position
    Add start and finish time
    Add related functionality to these
    
    Co-authored-by: MaximGonnissen <33728489+maximgonnissen@users.noreply.github.com>

--- src/main/java/WarehouseShared/Job.java
+++ src/main/java/WarehouseShared/Job.java
@@ -2,27 +2,82 @@ package WarehouseShared;
 
 import Enums.JobType;
 
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Job {
-    public JobType action;
-    //TODO fix with tuples or something
-    public ArrayList<ArrayList<Integer>> path;
+    protected int id;
+    protected JobType action;
+    protected ArrayList<Position> path;
+    protected Position sourcePosition;
+    public LocalTime startTime;
+    public LocalTime finishedTime;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setStartTime() {
+        setStartTime(LocalTime.now());
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public void setFinishedTime() {
+        setFinishedTime(LocalTime.now());
+    }
+
+    public void setFinishedTime(LocalTime finishedTime) {
+        this.finishedTime = finishedTime;
+    }
+
+    public Duration jobDuration() {
+        return Duration.between(this.startTime, this.finishedTime);
+    }
 
     public String getAction() {
         return action.toString();
     }
 
     public void setAction(String action) {
-        this.action = JobType.valueOf(action);
+        this.setAction(JobType.valueOf(action));
     }
 
-    public ArrayList<ArrayList<Integer>> getPath() {
+    public void setAction(JobType action) {
+        this.action = action;
+    }
+
+    public Position getSourcePosition() {
+        return this.sourcePosition;
+    }
+
+    public void setSourcePosition(Position position) {
+        this.sourcePosition = position;
+    }
+
+    public ArrayList<Position> getPath() {
         return path;
     }
 
-    public void setPath(ArrayList<ArrayList<Integer>> path) {
+    public void setPath(ArrayList<Position> path) {
         this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return getAction();
+    }
+
+    //TO DO: Add payload information
+    public String getJsonString() {
+        return "";
     }
 }
 
