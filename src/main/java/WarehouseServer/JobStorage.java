6f4b7fbeb6f9371b937280dc52473a3e81da4ef8
ThimotyS <thimoty.smet@gmail.com>
2022-12-19 19:11:20 +0100

    Add fail job method to JobStorage.java
    Co-authored-by: MaximGonnissen <33728489+maximgonnissen@users.noreply.github.com>

--- src/main/java/WarehouseServer/JobStorage.java
+++ src/main/java/WarehouseServer/JobStorage.java
@@ -2,6 +2,7 @@ package WarehouseServer;
 
 import WarehouseShared.Job;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -16,13 +17,13 @@ public class JobStorage {
     public static ArrayList<Job> inProgressJobs = new ArrayList<>();
     public static ArrayList<Job> finishedJobs = new ArrayList<>();
 
-    public static void addToDoJob(Job job){
+    public static void addToDoJob(Job job) {
         System.out.println("Adding Job to Queue" + job.toString());
         toDoJobs.add(job);
         System.out.println("Added a Job to the Queue: " + toDoJobs.get(toDoJobs.size() - 1));
     }
 
-    public static void addInProgressJob(Job job){
+    public static void addInProgressJob(Job job) {
         if(!toDoJobs.contains(job)){
             System.out.println("Job does not exist in to-do Jobs");
             return;
@@ -32,21 +33,21 @@ public class JobStorage {
         toDoJobs.remove(job);
         System.out.println("Job removed from to-do Jobs");
 
-        System.out.println("Adding Job to in-progress Jobs: " + job);
+        System.out.println("Adding Job to in-progress Jobs: " + job.toString());
         inProgressJobs.add(job);
-        System.out.println("Added a Job to the in-progress list: " + inProgressJobs.get(inProgressJobs.size() - 1));
+        System.out.println("Added a Job to the in-progress Jobs: " + inProgressJobs.get(inProgressJobs.size() - 1));
 
         // Todo: Also set this if it already had a start time? (For example when job is handed back to the system for whatever reason)
         job.setStartTime();
     }
 
-    public static void addFinishedJob(RobotObject robot, Job job){
+    public static void addFinishedJob(RobotObject robot, Job job) {
         if(!inProgressJobs.contains(job)){
             System.out.println("Job does not exist in in-progress Jobs");
             return;
         }
 
-        System.out.println("Robot: " + robot.toString() + " finished job: " + job);
+        System.out.println("Robot: " + robot.toString() + " finished job: " + job.toString());
 
         System.out.println("Removing Job from in-progress Jobs");
         inProgressJobs.remove(job);
@@ -54,11 +55,44 @@ public class JobStorage {
 
         System.out.println("Adding Job to finished Jobs: " + job);
         finishedJobs.add(job);
-        System.out.println("Added a Job to the finished list: " + finishedJobs.get(finishedJobs.size() - 1));
+        System.out.println("Added a Job to the finished Jobs: " + finishedJobs.get(finishedJobs.size() - 1));
 
         job.setFinishedTime();
 
         System.out.println("Job duration: " + job.jobDuration().toString());
     }
 
+    public static void failJob(RobotObject robot, Job job) {
+        if(!inProgressJobs.contains(job)){
+            System.out.println("Job does not exist in in-progress Jobs");
+            return;
+        }
+
+        System.out.println("Robot: " + robot.toString() + "failed job: " + job.toString());
+
+        System.out.println("Removing Job from in-progress Jobs");
+        inProgressJobs.remove(job);
+        System.out.println("Job removed from in-progress Jobs");
+
+        System.out.println("Adding Job back to to-do jobs: " + job);
+        toDoJobs.add(job);
+        System.out.println("Added a Job to the to-do list: " + toDoJobs.get(toDoJobs.size() - 1));
+    }
+
+    public static ArrayList<Job> allJobs() {
+        ArrayList<Job> joinedList = new ArrayList<>();
+        joinedList.addAll(toDoJobs);
+        joinedList.addAll(inProgressJobs);
+        joinedList.addAll(finishedJobs);
+        return joinedList;
+    }
+    
+    public static Job getFromId(int id) {
+        for (Job job : allJobs()) {
+            if (job.getId() == id) {
+                return job;
+            }
+        }
+        return null;
+    }
 }
