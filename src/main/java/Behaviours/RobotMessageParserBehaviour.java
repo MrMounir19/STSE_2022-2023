63572ec9e16ca1eedb943a387fe56337569d5646
anfeny <peetersanthony208@gmail.com>
2022-12-20 22:09:15 +0100

    Refactor RobotMessageParserBehaviour
    Make serverAgentName static
    Removed redundant print statements

--- src/main/java/Behaviours/RobotMessageParserBehaviour.java
+++ src/main/java/Behaviours/RobotMessageParserBehaviour.java
@@ -4,10 +4,6 @@ import Enums.MessageType;
 import WarehouseShared.Job;
 import Utils.Messages;
 import WarehouseRobot.RobotInformation;
-import WarehouseShared.Position;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
@@ -23,7 +19,7 @@ import java.util.Objects;
  * @since 26/11/2022
  */
 public class RobotMessageParserBehaviour extends CyclicBehaviour {
-    String serverAgentName = null;
+    static String serverAgentName = null;
 
     @Override
     public void action() {
@@ -35,13 +31,12 @@ public class RobotMessageParserBehaviour extends CyclicBehaviour {
     }
 
     private void handleMessage(ACLMessage message) {
-        if (this.serverAgentName != null) {
-            if (!Objects.equals(message.getSender().getLocalName(), this.serverAgentName)) {
+        if (serverAgentName != null) {
+            if (!Objects.equals(message.getSender().getLocalName(), serverAgentName)) {
                 System.out.println("Server agent name mismatch. Discarding message.");
                 return;
             }
         }
-        System.out.println("received message");
         String content = message.getContent();
         JsonObject payload;
 
@@ -51,16 +46,12 @@ public class RobotMessageParserBehaviour extends CyclicBehaviour {
             e.printStackTrace();
             return;
         }
-        System.out.println("Parsing message type");
-        System.out.println(payload);
         MessageType messageType = MessageType.valueOf(payload.get("messageType").getAsString());
-        System.out.println("Done parsing message type");
 
         if (messageType == MessageType.RegistrationConfirmation) {
             handleRegistrationConfirmationMessage(message);
 
         } else if (messageType == MessageType.Job) {
-            System.out.println("Start check");
             handleJobMessage(message);
         } else {
             System.out.println("Received message type not valid for robot.");
@@ -74,7 +65,6 @@ public class RobotMessageParserBehaviour extends CyclicBehaviour {
     }
 
     private void handleJobMessage(ACLMessage message) {
-        // TODO: Handle if already working on a job.
         JsonObject payload = Messages.toJson(message.getContent());
         JsonObject data = payload.getAsJsonObject("data");
 
